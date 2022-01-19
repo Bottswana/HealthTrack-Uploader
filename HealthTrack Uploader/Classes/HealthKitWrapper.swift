f4f64a0be9a6e60dbc91118e992a8ebb73276c60
James Botting <james@bottswanamedia.info>
2022-01-19 21:30:46 +0000

    Play around with UIKit, retrieve data from HealthKit

--- HealthTrack Uploader/Classes/HealthKitWrapper.swift
+++ HealthTrack Uploader/Classes/HealthKitWrapper.swift
@@ -15,16 +15,75 @@ class HealthKitWrapper
         case notAvailableOnDevice
         case dataTypeNotAvailable
     }
+    
+    enum HealthKitQueryError: Error
+    {
+        case unknownError
+    }
   
     class func authoriseHealthKit() async throws -> Swift.Void
     {
-        // See if we have HealthKit on the device we are running on
+        // Fail if we dont have HealthKit on this device
         guard HKHealthStore.isHealthDataAvailable() else
         {
             throw HealthKitSetupError.notAvailableOnDevice;
         }
         
-        // See if we have the data types we are interested in
+        // Request the data types we are interested in
+        let dataTypes = try getDataTypes();
+        try await HKHealthStore().__requestAuthorization(toShare: nil, read: dataTypes);
+    }
+    
+    class func isAuthorised() async -> Bool
+    {
+        do
+        {
+            let dataTypes = try getDataTypes();
+            let authorisationResult = try await HKHealthStore().statusForAuthorizationRequest(toShare: [], read: dataTypes);
+            return authorisationResult == HKAuthorizationRequestStatus.unnecessary;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+    
+    class func getSamplesForCurrentDay(dataType: HKQuantityType, options: HKStatisticsOptions? = nil) async throws -> HKStatistics
+    {
+        // Setup the scope for our samples
+        let timeNow = Date();
+        let mostRecentPredicate = HKQuery.predicateForSamples(withStart: Calendar.current.startOfDay(for: timeNow), end: timeNow, options: .strictEndDate);
+    
+        // Convert continuation based api to modern standards
+        return try await withCheckedThrowingContinuation
+        { continuation in
+            let dataQuery = HKStatisticsQuery(quantityType: dataType, quantitySamplePredicate: mostRecentPredicate, options: options ?? [])
+            { (_, dataWrapped, error) in
+                guard let data = dataWrapped else
+                {
+                    // Error in continuation
+                    if let errorunwrap = error
+                    {
+                        continuation.resume(throwing: errorunwrap);
+                        return;
+                    }
+                    
+                    // Unable to unwrap error
+                    continuation.resume(throwing: HealthKitQueryError.unknownError);
+                    return;
+                }
+                
+                // Return data
+                continuation.resume(returning: data)
+            }
+            
+            // Execute Query
+            HKHealthStore().execute(dataQuery);
+        }
+    }
+    
+    private class func getDataTypes() throws -> Set<HKSampleType>
+    {
         guard let exerciseMinutes = HKObjectType.quantityType(forIdentifier: .appleExerciseTime),
               let restingHeart = HKObjectType.quantityType(forIdentifier: .restingHeartRate),
               let stepCount = HKObjectType.quantityType(forIdentifier: .stepCount)
@@ -33,13 +92,10 @@ class HealthKitWrapper
             throw HealthKitSetupError.dataTypeNotAvailable;
         }
         
-        // Setup access to HealthKit Data
-        let healthKitTypesToRead: Set<HKSampleType> = [
+        return [
             exerciseMinutes,
             restingHeart,
             stepCount
         ];
-
-        try await HKHealthStore().__requestAuthorization(toShare: nil, read: healthKitTypesToRead);
     }
 }
--- HealthTrack Uploader/View Controllers/HealthDataControler.swift
+++ HealthTrack Uploader/View Controllers/HealthDataControler.swift
@@ -7,10 +7,27 @@
 //
 
 import Foundation
+import HealthKit
 import UIKit
 
 class HealthDataController: UITableViewController
 {
+    private var isAuthorised: Bool = false;
+    
+    override func loadView()
+    {
+        super.loadView();
+        Task.init
+        {
+            let authStatus = await HealthKitWrapper.isAuthorised();
+            if( authStatus )
+            {
+                // Update the view to match that we don't need to request authorisation again
+                updateViewHealthKitAuthorised();
+            }
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
         guard ( indexPath.section != 0 ) else
@@ -18,34 +35,57 @@ class HealthDataController: UITableViewController
             switch indexPath.row
             {
                 case 0:
-                    // Authorise HealthKit
-                    Task.init
+                    // Authorise HealthKit Button
+                    guard isAuthorised else
                     {
-                        do
+                        updateLabelWithColour(path: IndexPath(row: 0, section: 0), colour: UIColor.secondaryLabel);
+                        updateAccessoryWithActivityIndicator(path: IndexPath(row: 0, section: 0));
+                        isAuthorised = true;
+                        Task.init
                         {
-                            try await HealthKitWrapper.authoriseHealthKit();
-                            
-                            
-
+                            do
+                            {
+                                try await HealthKitWrapper.authoriseHealthKit();
+                                updateViewHealthKitAuthorised();
+                            }
+                            catch HealthKitWrapper.HealthKitSetupError.dataTypeNotAvailable
+                            {
+                                throwErrorDialog(errorText: "Authorization of HealthKit failed:\nThis device does not support the required data");
+                                updateAccessoryWithNothing(path: IndexPath(row: 0, section: 0), colour: UIColor.tintColor);
+                                updateLabelWithColour(path: IndexPath(row: 0, section: 0), colour: UIColor.tintColor);
+                                isAuthorised = false;
+                            }
+                            catch HealthKitWrapper.HealthKitSetupError.notAvailableOnDevice
+                            {
+                                throwErrorDialog(errorText: "Authorization of HealthKit failed:\nThis device does not support HealthKit");
+                                updateAccessoryWithNothing(path: IndexPath(row: 0, section: 0), colour: UIColor.tintColor);
+                                updateLabelWithColour(path: IndexPath(row: 0, section: 0), colour: UIColor.tintColor);
+                                isAuthorised = false;
+                            }
+                            catch
+                            {
+                                throwErrorDialog(errorText: "Authorization of HealthKit failed:\n\(error)");
+                                updateAccessoryWithNothing(path: IndexPath(row: 0, section: 0), colour: UIColor.tintColor);
+                                updateLabelWithColour(path: IndexPath(row: 0, section: 0), colour: UIColor.tintColor);
+                                isAuthorised = false;
+                            }
                         }
-                        catch HealthKitWrapper.HealthKitSetupError.dataTypeNotAvailable
-                        {
-                            throwErrorDialog(errorText: "Authorization of HealthKit failed:\nThis device does not support the required data");
-                        }
-                        catch HealthKitWrapper.HealthKitSetupError.notAvailableOnDevice
-                        {
-                            throwErrorDialog(errorText: "Authorization of HealthKit failed:\nThis device does not support HealthKit");
-                        }
-                        catch
-                        {
-                            throwErrorDialog(errorText: "Authorization of HealthKit failed:\n\(error)");
-                        }
+                        
+                        return;
                     }
                 break;
                 case 1:
                     // Refresh Data
-                
-                
+                    guard !isAuthorised else
+                    {
+                        Task.init
+                        {
+                            let updateResults = await refreshHealthKitData();
+                            print("Results: \(updateResults)");
+                        }
+                        
+                        return;
+                    }
                 break;
                 default:
                     return;
@@ -55,10 +95,150 @@ class HealthDataController: UITableViewController
         }
     }
     
-    func throwErrorDialog(errorText: String)
+    func refreshHealthKitData() async -> (activeMinutes: Double?, numberSteps: Double?, restingHeartRate: Double?)
+    {
+        var healthKitActiveMinutes: Double? = nil;
+        var healthKitStepCount: Double? = nil;
+        var healthKitRestingHR: Double? = nil;
+        
+        // Retrieve Exercise Minutes
+        do
+        {
+            let exersiseTimeTypeWrapped = HKObjectType.quantityType(forIdentifier: .appleExerciseTime);
+            if let exersiseTimeType = exersiseTimeTypeWrapped
+            {
+                let queryResult = try await HealthKitWrapper.getSamplesForCurrentDay(dataType: exersiseTimeType, options: .cumulativeSum);
+                let todaysSampleWrapped = queryResult.sumQuantity();
+                if let todaysSample = todaysSampleWrapped
+                {
+                    let rawValue = todaysSample.doubleValue(for: HKUnit.minute());
+                    //print("HealthKitActiveMinutes: \(rawValue)");
+                    healthKitActiveMinutes = rawValue;
+                }
+            }
+        }
+        catch
+        {
+            print("No samples for HealthKitActiveMinutes");
+        }
+        
+        // Retrieve Step Count
+        do
+        {
+            let stepCountTypeWrapped = HKObjectType.quantityType(forIdentifier: .stepCount);
+            if let stepCountType = stepCountTypeWrapped
+            {
+                let queryResult = try await HealthKitWrapper.getSamplesForCurrentDay(dataType: stepCountType, options: .cumulativeSum);
+                let todaysSampleWrapped = queryResult.sumQuantity();
+                if let todaysSample = todaysSampleWrapped
+                {
+                    let rawValue = todaysSample.doubleValue(for: HKUnit.count());
+                    //print("HealthKitStepCount: \(rawValue)");
+                    healthKitStepCount = rawValue;
+                }
+            }
+        }
+        catch
+        {
+            print("No samples for HealthKitStepCount");
+        }
+        
+        // Retrieve Avg Resting Heartrate
+        do
+        {
+            let restingHRTypeWrapped = HKObjectType.quantityType(forIdentifier: .restingHeartRate);
+            if let restingHRType = restingHRTypeWrapped
+            {
+                let queryResult = try await HealthKitWrapper.getSamplesForCurrentDay(dataType: restingHRType, options: .discreteAverage);
+                let todaysSampleWrapped = queryResult.averageQuantity();
+                if let todaysSample = todaysSampleWrapped
+                {
+                    let rawValue = todaysSample.doubleValue(for: HKUnit.init(from: "count/min"));
+                    //print("HealthKitAvgHR: \(rawValue)");
+                    healthKitRestingHR = rawValue;
+                }
+            }
+        }
+        catch
+        {
+            print("No samples for HealthKitAvgHR");
+        }
+
+        // Return data
+        return (healthKitActiveMinutes, healthKitStepCount, healthKitRestingHR);
+    }
+    
+    private func throwErrorDialog(errorText: String)
     {
         let failedAlert = UIAlertController(title: "Error", message: errorText, preferredStyle: .alert);
         failedAlert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default));
         self.present(failedAlert, animated: true, completion: nil);
+    }
+    
+    private func updateAccessoryWithActivityIndicator(path: IndexPath)
+    {
+        let healthKitCellWrap = tableView.cellForRow(at: path);
+        if let healthKitCell = healthKitCellWrap
+        {
+            // Create the ActivityIndicatorView
+            let activityIndicator = UIActivityIndicatorView();
+            activityIndicator.frame = CGRect(x: 0, y: 0, width: 24, height: 24);
+            
+            // Bind the view to the TableCell Accessory
+            healthKitCell.accessoryView = activityIndicator;
+            healthKitCell.isUserInteractionEnabled = false;
+            activityIndicator.startAnimating();
+        }
+    }
+    
+    private func updateAccessoryWithCheck(path: IndexPath, colour: UIColor)
+    {
+        let healthKitCellWrap = tableView.cellForRow(at: path);
+        if let healthKitCell = healthKitCellWrap
+        {
+            healthKitCell.accessoryType = UITableViewCell.AccessoryType.checkmark;
+            healthKitCell.isUserInteractionEnabled = false;
+            healthKitCell.accessoryView = nil;
+            healthKitCell.tintColor = colour;
+            healthKitCell.isSelected = false;
+        }
+    }
+    
+    private func updateAccessoryWithNothing(path: IndexPath, colour: UIColor)
+    {
+        let healthKitCellWrap = tableView.cellForRow(at: path);
+        if let healthKitCell = healthKitCellWrap
+        {
+            healthKitCell.accessoryType = UITableViewCell.AccessoryType.none;
+            healthKitCell.isUserInteractionEnabled = true;
+            healthKitCell.accessoryView = nil;
+            healthKitCell.tintColor = colour;
+        }
+    }
+    
+    private func updateLabelWithColour(path: IndexPath, colour: UIColor)
+    {
+        let healthKitCellWrap = tableView.cellForRow(at: path);
+        if let healthKitCell = healthKitCellWrap
+        {
+            if let cellLabel = healthKitCell.viewWithTag(1) as? UILabel
+            {
+                cellLabel.highlightedTextColor = colour;
+                cellLabel.isHighlighted = false;
+                cellLabel.textColor = colour;
+            }
+        }
+    }
+    
+    private func updateViewHealthKitAuthorised()
+    {
+        // Update HealthKit button
+        updateAccessoryWithCheck(path: IndexPath(row: 0, section: 0), colour: UIColor.secondaryLabel);
+        updateLabelWithColour(path: IndexPath(row: 0, section: 0), colour: UIColor.secondaryLabel);
+        
+        // Update Sync Button
+        updateAccessoryWithNothing(path: IndexPath(row: 1, section: 0), colour: UIColor.tintColor);
+        updateLabelWithColour(path: IndexPath(row: 1, section: 0), colour: UIColor.tintColor);
+        isAuthorised = true;
     }
 }
