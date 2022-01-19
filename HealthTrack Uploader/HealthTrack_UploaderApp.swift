6fa09921d45955bfc5c7d3d562b4bf26d537fdd3
James Botting <james@bottswanamedia.info>
2022-01-19 00:35:51 +0000

    Initial commit of Swift/UIKit

---
+++ HealthTrack Uploader/HealthTrack_UploaderApp.swift
@@ -0,0 +1,20 @@
+//
+//  HealthTrack_UploaderApp.swift
+//  HealthTrack Uploader
+//
+//  Created by James Botting on 18/01/2022.
+//  Copyright © 2022 Bottswana Media. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    var window: UIWindow?
+  
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        return true
+    }
+}
